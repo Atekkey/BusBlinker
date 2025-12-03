1cae45add77c74ff3ade5b3a5ca8a68636f561ba
Atekkey <anand@tekkey.net>
2025-12-03 14:32:50 -0600

    i1

---
+++ img.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+import os
+import time
+from datetime import datetime
+import digit_maps
+from PIL import Image
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from samplebase import SampleBase
+import numpy as np
+
+
+class test(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(test, self).__init__(*args, **kwargs)
+    
+
+    def run(self):
+        img = Image.open("crest.jpg").convert("RGB")
+        img2 = img.rotate(90) 
+        canvas = self.matrix.CreateFrameCanvas()
+        while True:
+            canvas.SetImage(img2)
+            canvas = self.matrix.SwapOnVSync(canvas) # Refreshes the canvas
+            time.sleep(1)
+
+
+
+# Main function
+if __name__ == "__main__":
+    simple_square = test()
+    if (not simple_square.process()):
+        simple_square.print_help()
