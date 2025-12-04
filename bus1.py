3ac402ffc3b6b5d377ce274eb9ed47280b8dedcd
Atekkey <anand@tekkey.net>
2025-12-04 13:48:50 -0600

    Thu

--- bus1.py
+++ bus1.py
@@ -104,7 +104,7 @@ class test(SampleBase):
                 elif array[y, x] == 6:
                     canvas.SetPixel(x, y, 178, 232, 116) # lg
                 elif array[y, x] == 7:
-                    canvas.SetPixel(x, y, 5, 86, 165) # uiuc B
+                    canvas.SetPixel(x, y, 5+20, 86+20, 165+20) # uiuc B
                 elif array[y, x] == 8:
                     canvas.SetPixel(x, y, 255, 95, 5) # uiuc OR
                 else:
