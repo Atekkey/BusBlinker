ea78e1f30f0d56979f628f1b0c2ec0bc5ee3a48f
Atekkey <anand@tekkey.net>
2025-12-04 00:22:08 -0600

    none

--- bus1.py
+++ bus1.py
@@ -69,7 +69,7 @@ def makeRegMatrix():
     date = now.strftime("%m/%d")
     day_of_week = now.strftime("%a")
     
-    day_of_week = MAP[day_of_week]
+    day_of_week = MAP[day_of_week.upper()].upper()
     draw_string(arrCanv, day_of_week.upper(), off + 10*3, 0, color=1)
     draw_string(arrCanv, date, off + 10*4, 0, color=5)
     hour_min = now.strftime("%I:%M")
