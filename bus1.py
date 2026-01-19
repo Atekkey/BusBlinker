85ec85c5c36080481fac9178592413a92fd0677c
Atekkey <anand@tekkey.net>
2026-01-19 15:56:25 -0600

    dotw

--- bus1.py
+++ bus1.py
@@ -103,7 +103,7 @@ def makeRegMatrix(fetch_weather):
     # TIME INFO
     now = datetime.now()
     date = now.strftime("%m/%d")
-    day_of_week = now.strftime("%a")
+    day_of_week = now.strftime("%a")[0]
     hour_min = now.strftime("%I:%M")
     draw_string(arrCanv, day_of_week.upper().strip(), off + 10*3, 7, color=6) # FRI
     draw_string(arrCanv, date, off + 10*4, 0, color=5) # 12/05
