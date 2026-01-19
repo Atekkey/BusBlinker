0e899fd27787b74e10b4937b3b932f4b582e4629
Atekkey <anand@tekkey.net>
2026-01-19 16:08:10 -0600

    Bus1 edit

--- bus1.py
+++ bus1.py
@@ -104,7 +104,7 @@ def makeRegMatrix(fetch_weather):
     dowMap = {"MON": "M", "TUE": "T", "WED": "W", "THU": "H", "FRI": "F", "SAT": "S", "SUN": "U"}
     day_of_week = dowMap.get(day_of_week, day_of_week)
     hour_min = now.strftime("%I:%M")
-    draw_string(arrCanv, day_of_week, off + 10*3, 20, color=6) # FRI
+    draw_string(arrCanv, day_of_week, off + 10*3, 25, color=6) # FRI
     draw_string(arrCanv, date, off + 10*4, 1, color=5) # 12/05
     draw_string(arrCanv, hour_min, off + 10*5, 2, color=5) # 01:04
     
